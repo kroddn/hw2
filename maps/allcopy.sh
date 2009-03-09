9961a06d6d22aff338c4fb05a6321799912935f6
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-03-09 15:06:08 +0000

    Kopier-Ziel korrigiert

--- maps/allcopy.sh
+++ maps/allcopy.sh
@@ -6,9 +6,9 @@ if [ ! -f $q ]; then
 fi
 
 while [ $# -gt 0 ]; do
-    echo "game2 "; scp $1 kroddn@mysql:/home/hw2_game1/html/admintools/$1 &
-    echo "game1 "; scp $1 kroddn@mysql:/home/hw2_game2/html/admintools/$1 &
-    echo "speed "; scp $1 kroddn@mysql:/home/hw2_speed/html/admintools/$1
+    echo "game2 "; scp $1 kroddn@mysql:/home/hw2_game1/html/maps/$1 &
+    echo "game1 "; scp $1 kroddn@mysql:/home/hw2_game2/html/maps/$1 &
+    echo "speed "; scp $1 kroddn@mysql:/home/hw2_speed/html/maps/$1
     shift
 done
 

619401b971439bc11c8e81bfc4e7508757a76385
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-03-22 16:37:38 +0000

    SF.net migration

---
+++ trunk/html/includes/banner2.inc.php
@@ -0,0 +1,20 @@
+<?php
+// This file should be implemented.
+// On holy-wars2.de, this file is taken from project
+// "strength-and-honor-game.de"
+
+function printBanner($id = null, $height=60, $width=468) {
+  $magic = "magic22445";
+  $border = 1;
+  
+  echo "<div valign='middle' id='".$magic."' border='4' name='".$magic."'".
+       " framespacing='0' frameborder='no' scrolling='no' style='width: ".$width."; height:".$height."; border: ${border}px solid black;'>\n";
+  
+  echo "ADS IMPLEMENT ME";
+  
+  echo "\n</div>\n";
+
+}
+
+
+?>

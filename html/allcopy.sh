19e97c1e286c4ba654d8f869c4f7bea588a3dd85
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-06-06 08:46:56 +0000

    HiSpeed-Anpassungen. - FavIcon - Verwaltung 6 fixes

--- html/allcopy.sh
+++ html/allcopy.sh
@@ -6,9 +6,10 @@ if [ ! -f $q ]; then
 fi
 
 while [ $# -gt 0 ]; do
-    echo "game2 "; scp $1 kroddn@mysql:/home/hw2_game1/html/$1 &
-    echo "game1 "; scp $1 kroddn@mysql:/home/hw2_game2/html/$1 &
-    echo "speed "; scp $1 kroddn@mysql:/home/hw2_speed/html/$1
+    echo "game1   "; scp $1 kroddn@www4:/home/hw2_game1/html/$1 &
+    echo "game2   "; scp $1 kroddn@www4:/home/hw2_game2/html/$1 &
+    echo "speed   "; scp $1 kroddn@www4:/home/hw2_speed/html/$1 &
+    echo "hispeed "; scp $1 kroddn@www4:/home/hw2_hispeed/html/$1
     shift
 done
 

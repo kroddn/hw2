09a3518aa1a9e26548c998e90848509fb3d92f99
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-06-26 13:31:20 +0000

    Reset-Skripte / Kartenerstellungsskripte

---
+++ trunk/skripte/import.config.php
@@ -0,0 +1,18 @@
+<?
+// Diese Datei nur auf dem Server anpassen!
+
+define("PERCENT_WOOD",  15);
+define("PERCENT_STONE", 15);
+
+define("AMOUNT_SPECIAL_FISH",  2000);
+define("AMOUNT_SPECIAL_PEARLS", 2000);
+
+define("AMOUNT_SPECIAL_WINE",  3500);
+define("AMOUNT_SPECIAL_WHEAT",  3500);
+
+define("AMOUNT_SPECIAL_FURBS", 3500);
+define("AMOUNT_SPECIAL_HERBS", 3500);
+
+define("AMOUNT_SPECIAL_METAL", 3500);
+define("AMOUNT_SPECIAL_GEMS",  3500);
+?>

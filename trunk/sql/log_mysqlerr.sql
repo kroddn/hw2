c256099a61408ad3e65bcc1bb80e61604e79234d
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-07-10 21:00:32 +0000

    SQL-file log_mysqlerr.sql

---
+++ trunk/sql/log_mysqlerr.sql
@@ -0,0 +1,17 @@
+--
+-- Tabellenstruktur für Tabelle `log_mysqlerr`
+--
+
+CREATE TABLE IF NOT EXISTS `log_mysqlerr` (
+  `id` int(11) NOT NULL auto_increment,
+  `qry` text collate latin1_german2_ci NOT NULL,
+  `err` text collate latin1_german2_ci NOT NULL,
+  `time` int(11) NOT NULL default '0',
+  `scriptname` text collate latin1_german2_ci,
+  `referer` text collate latin1_german2_ci NOT NULL,
+  `fixed` int(11) default NULL,
+  `player` int(11) default NULL,
+  `get_str` text collate latin1_german2_ci,
+  `post_str` text collate latin1_german2_ci,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci AUTO_INCREMENT=1 ;

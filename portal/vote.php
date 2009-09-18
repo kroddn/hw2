10665c7d4f64f7b8036473a7c47ad5ad8444859f
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-09-18 10:59:41 +0000

    Vote button corrections.

--- portal/vote.php
+++ portal/vote.php
@@ -1,3 +1,32 @@
+<?
+/*************************************************************************
+    This file is part of "Holy-Wars 2" 
+    http://holy-wars2.de / https://sourceforge.net/projects/hw2/
+
+    Copyright (C) 2003-2009 
+    by Markus Sinner, Gordon Meiser, Laurenz Gamper, Stefan Neubert
+
+    "Holy-Wars 2" is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Former copyrights see below.
+ **************************************************************************/
+
+// Careful. This needs "includes/vote.inc.php" from folder "includes" in html Sources.
+
+// This must be defined for include to works
+define("GFX_PATH_LOCAL", "");
+?>
 <html>
 <head>
 <title>Vote 4 HW2</title>
@@ -9,7 +38,7 @@
 <p>
 <table><tr><td width="400">
 <?
-include("vote.inc.php");
+include("includes/vote.inc.php");
 ?>
 </td></tr></table>
 </center>

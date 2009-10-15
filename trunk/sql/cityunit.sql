8aef18b00094e2c976578b67d14ab28d43a01bbc
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2009-10-15 17:59:41 +0000

    Vorbereitung für herrenlose Städte * Tabelle city und cityunit anpassen * Funktion "abandoneCity" zum herrenlos-machen eingeführt

--- trunk/sql/cityunit.sql
+++ trunk/sql/cityunit.sql
@@ -3,9 +3,9 @@
 --
 
 CREATE TABLE IF NOT EXISTS `cityunit` (
-  `city` int(10) NOT NULL default '0',
-  `unit` int(10) NOT NULL default '0',
+  `city` int(10) NOT NULL,
+  `unit` int(10) NOT NULL,
   `count` int(10) NOT NULL default '0',
-  `owner` int(10) NOT NULL default '0',
-  PRIMARY KEY  (`city`,`unit`,`owner`)
+  `owner` int(10) NULL,
+  UNIQUE (`city`,`unit`,`owner`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_german2_ci;

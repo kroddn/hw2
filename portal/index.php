7af67c58789af17c77dde290e86154e656ed2300
kroddn <kroddn@4ba0e9f8-c9b0-4973-bfac-880f5db3efbc>
2010-04-12 13:30:53 +0000

    * Kopierscripte angepasst * Portal update

--- portal/index.php
+++ portal/index.php
@@ -407,8 +407,8 @@ onClick="return framelayer('http://www.holy-wars2.de/cgi-bin/cgiirc/irc.cgi')">
 
     <DIV id="shortnews">
       <font color="#8FFF4F">
-      <b>&quot;HW2 Revial&quot;</b> - neue Hauptrunde am 16. Oktober</font><br>
-      Weitere Informationen <a target="_blank" href="http://forum.holy-wars2.de/viewtopic.php?f=1&amp;t=10915">hier im Forum</a>.      
+      <b>Neue Hauptrunde am 19. Februar 2010</font><br>
+      Weitere Informationen <a target="_blank" href="http://forum.holy-wars2.de/viewtopic.php?f=1&t=11038">hier im Forum</a>.      
     </DIV>
 
 

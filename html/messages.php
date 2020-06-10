238fc97508d7cf8dd01eadefd609088204338a02
Kroddn <sinner@psitronic.de>
2020-06-10 18:19:32 +0200

    Fix mysql_affected_rows => mysqli_

--- html/messages.php
+++ html/messages.php
@@ -49,7 +49,7 @@ if(isset($delmarked) && sizeof($mark)>0){
     $sql.=")";
     do_mysql_query($sql);
     // Wurden welche gelöscht, dann Postausgang neu berechnen
-    if (mysql_affected_rows() > 0) {
+    if (mysqli_affected_rows() > 0) {
       $player->messages_sent = -1;
     }
   }
